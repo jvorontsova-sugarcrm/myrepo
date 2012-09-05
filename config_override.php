5ff099f8094d79d797b8c3aaa24b4469c23c13ae
jvorontsova <jvorontsova@sugarcrm.com>
2012-09-05 01:50:51 -0700

    config_override.php

---
+++ config_override.php
@@ -0,0 +1,14 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['http_referer']['weak'] = true;
+$sugar_config['disable_autocomplete'] = true;
+$sugar_config['use_sprites'] = false;
+$sugar_config['passwordsetting']['minpwdlength'] = '3';
+$sugar_config['passwordsetting']['onelower'] = '0';
+$sugar_config['passwordsetting']['onenumber'] = '0';
+$sugar_config['passwordsetting']['onespecial'] = '0';
+$sugar_config['passwordsetting']['SystemGeneratedPasswordON'] = '0';
+$sugar_config['SAML_loginurl'] = '';
+$sugar_config['SAML_X509Cert'] = '';
+$sugar_config['authenticationClass'] = '';
+/***CONFIGURATOR***/
